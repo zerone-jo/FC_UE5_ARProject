16f8494bf8041e7a6c8c3f513b640bdd1a0b7e72
DESKTOP-R4OL3TN\DESKTOP <91.jaewon@gmail.com>
2023-09-26 11:19:22 +0900

    프로젝트 세팅

---
+++ Source/ARProject/ARProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARPROJECT_API AARProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
